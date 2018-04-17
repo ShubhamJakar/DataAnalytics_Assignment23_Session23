d9999de9eab1772ab211bf45270e8396fac30d43
Shubham Jakar <34440552+ShubhamJakar@users.noreply.github.com>
2018-04-17 23:27:16 +0530

    Add files via upload

---
+++ Assignment23_session23_Answer.R
@@ -0,0 +1,10 @@
+#Assignment23_Session23
+
+#Problem
+#1. Perform the below given activities:
+#a. Take Apple Stock Prices from Yahoo Finance for last 90 days
+#b. Predict the Stock closing prices for next 15 days.
+#c. Submit your accuracy
+#d. After 15 days again collect the data and compare with your forecast
+
+#Answers
